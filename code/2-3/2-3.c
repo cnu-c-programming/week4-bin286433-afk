27aaba7ab9309bfb56e4c14f0023e6ba592fecc8
bin286433-afk <bin286433@gmail.com>
2026-03-27 09:34:43 +0900

    Update 2-3.c

---
+++ code/2-3/2-3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+
+int main() {
+    char a[64] = "Hello";
+    char b[64] = "world";
+    char c[128];
+
+    int len = strlen(a);
+    printf("length of a: %d\n", len);
+
+    strcpy(c,a);
+    printf("a: %s, c: %s\n", a, c);
+    
+    strcat(c, " world");
+    printf("c: %s\n", c);
+
+    printf("strcmp(%s, %s) = %d\n", a, a, strcmp(a, "Hello"));
+    printf("strcmp(%s, %s) = %d\n", a, b, strcmp(b, "Hello"));
+
+
+
+    return 0;
+}
