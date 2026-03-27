35620847463b23e8297e41dadf48c44aa3022df0
bin286433-afk <bin286433@gmail.com>
2026-03-27 10:09:23 +0900

    Update 3-4.c

---
+++ code/3-4/3-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+void inx(int *x) {
+    if(*x == NULL){
+        return;
+    }
+    *x = *x+1;
+    printf("%d\n", *x);
+
+}
+
+
+int main() {
+    int x = 3;
+
+    inc(&x);
+    inc(NULL);
+
+
+    return 0;
+}
